5c6adc1d9fbd69a7c6e5c2575bb6ccceab5aa2cd
perlancar (on pc-office) <perlancar@gmail.com>
2018-01-09 15:35:39 +0700

    Initial commit

---
+++ lib/Pod/Weaver/Plugin/Acme/CPANModules.pm
@@ -0,0 +1,101 @@
+package Pod::Weaver::Plugin::Acme::CPANModules;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use Moose;
+with 'Pod::Weaver::Role::AddTextToSection';
+with 'Pod::Weaver::Role::Section';
+
+use Pod::From::Acme::CPANModules qw(gen_pod_from_acme_cpanmodules);
+
+sub _process_module {
+    no strict 'refs';
+
+    my ($self, $document, $input, $package) = @_;
+
+    my $filename = $input->{filename};
+
+    # XXX handle dynamically generated module (if there is such thing in the
+    # future)
+    local @INC = ("lib", @INC);
+
+    {
+        my $package_pm = $package;
+        $package_pm =~ s!::!/!g;
+        $package_pm .= ".pm";
+        require $package_pm;
+    }
+
+    my $res = gen_pod_from_acme_cpanmodules(
+        module => $package,
+        _raw=>1,
+    );
+
+    $self->add_text_to_section(
+        $document, $res->{list}, 'INCLUDED MODULES',
+        {after_section => ['DESCRIPTION']},
+    ) if $res->{list};
+
+    # XXX don't add if current See Also already mentions it
+    my @pod = (
+        "L<Acme::CPANModules> - about the Acme::CPANModules namespace\n\n",
+        "L<cpanmodules> - CLI tool to let you browse/view the lists\n\n",
+    );
+    $self->add_text_to_section(
+        $document, join('', @pod), 'SEE ALSO',
+        {after_section => ['DESCRIPTION']
+     },
+    );
+
+    $self->log(["Generated POD for '%s'", $filename]);
+}
+
+sub weave_section {
+    my ($self, $document, $input) = @_;
+
+    my $filename = $input->{filename};
+
+    my $package;
+    if ($filename =~ m!^lib/(.+)\.pm$!) {
+        $package = $1;
+        $package =~ s!/!::!g;
+        $self->_process_module($document, $input, $package);
+    }
+}
+
+1;
+# ABSTRACT: Plugin to use when building Acme::CPANModules::* distribution
+
+=for Pod::Coverage weave_section
+
+=head1 SYNOPSIS
+
+In your F<weaver.ini>:
+
+ [-Acme::CPANModules]
+
+
+=head1 DESCRIPTION
+
+This plugin is used when building Acme::CPANModules::* distributions. It
+currently does the following:
+
+=over
+
+=item * Create "INCLUDED MODULES" POD section from the list
+
+=item * Mention some modules in See Also section
+
+e.g. L<Acme::CPANModules> (the convention/standard), L<cpanmodules> (the CLI
+tool), etc.
+
+=back
+
+
+=head1 SEE ALSO
+
+L<Acme::CPANModules>
+
+L<Dist::Zilla::Plugin::Acme::CPANModules>
